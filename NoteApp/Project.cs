0d8a9ca9f043b756a91acd7f93d47003eeafe8d0
Andrei4 <meliciron@mail.ru>
2020-05-29 15:41:29 +0700

    Добавлена папка с проектом бизнес-логики: NoteApp

---
+++ NoteApp/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    /// <summary>
+    /// Класс, содержащий список (или словарь) всех заметок.
+    /// </summary>
+    public class Project
+    {
+        public List<Note> Notes { get; set; }
+    }
+}
---
+++ NoteApp/ProjectManager.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NoteApp
+{
+    /// <summary>
+    ///  Реализует метод для сохранения объекта «Проект» в файл и метод загрузки проекта из файла.
+    /// </summary>
+    public class ProjectManager
+    {
+    /// <summary>
+    /// переменная, содеражащая имя файла (де)сериализации.
+    /// </summary>
+        private string _filename;
+    /// <summary>
+    /// метод, получающий путь к папке appdata.
+    /// </summary>
+        private string _noteAppPath = Environment.ExpandEnvironmentVariables(@"%appdata%\NoteApp");
+        /// <summary>
+        /// метод, устанавливающий имя файла для работы с (де)сериализации.
+        /// </summary>
+        public void SetWorkFileName()
+        {
+            _filename = "Work.json"; 
+        }
+        /// <summary>
+        /// метод, устанавливающий имя файла для тестов (де)сериализации.
+        /// </summary>
+        public void SetTestFileName()
+        {
+            _filename = "Test.json";
+        }
+        /// <summary>
+        /// метод, сохраняющий данные о заметке в файл.
+        /// </summary>
+        /// <param name="project"></param>
+        public void SaveToFile(Project project)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamWriter sw = new StreamWriter(_noteAppPath + _filename))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, project);
+            }
+        }
+        /// <summary>
+        /// метод, возвращающий данные о заметке из файла.
+        /// </summary>
+        /// <returns></returns>
+        public Project LoadFromFile()
+        {
+            Project project = null;
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamReader sr = new StreamReader(_noteAppPath + _filename))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                project = (Project) serializer.Deserialize<Project>(reader);
+            }
+            return project;
+        }
+    }
+
+}
+
